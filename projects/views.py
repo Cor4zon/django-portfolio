594bf4d43a5c8954d85f1e7818bf55b769ee919a
Victor Fedorov <vpfedorov@bk.ru>
2021-10-12 13:44:53 +0300

    views update

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from projects.models import Project
+
 
 # Create your views here.
+def all_projects(request):
+    # query to DB to return all project objects
+    projects = Project.objects.all()
+    return render(request, "projects/all_projects.html", context={'projects': projects})
+
+
+def project_details(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, "projects/details.html", {'project': project})
+
+
+def project_list(request):
+    return render(request, "index.html")
+
