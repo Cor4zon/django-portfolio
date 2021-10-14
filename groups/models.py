06e34a906b5889c863c6a156ac78b52b61e6bd49
Victor Fedorov <vpfedorov@bk.ru>
2021-10-14 14:50:04 +0300

    AlbumCard model

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class AlbumCard(models.Model):
+    title = models.CharField(max_length=30)
+    artist = models.CharField(max_length=50)
+    year = models.DateField()
+    cover = models.FilePathField(path="/groups/img")
